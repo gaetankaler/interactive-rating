f911bf525c4b7105de75c7288f5d77291b0a2082
gaetankaler <gaga84120@gmail.com>
2022-10-27 11:36:48 +0200

    mise en place du design

---
+++ app.js
@@ -0,0 +1,11 @@
+let btn1 = document.querySelector("#clickBtn1");
+let btn2 = document.querySelector("#clickBtn2");
+let btn3 = document.querySelector("#clickBtn3");
+let btn4 = document.querySelector("#clickBtn4");
+let btn5 = document.querySelector("#clickBtn5");
+
+btn.addEventListener("click", () => {});
+
+btn1.addEventListener("mouseover", () => {
+  document.btn1.style.bagroundColor = "orange";
+});
